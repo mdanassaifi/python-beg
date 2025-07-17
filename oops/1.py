82be45450cd8726540df3b75a09be6955a5fbc7e
mdanassaifi <cscanas533@gmail.com>
2025-07-17 14:44:02 +0530

    xyz

--- oops/1.py
+++ oops/1.py
@@ -7,7 +7,15 @@ class car:
     def  full_name(self):
         return f"{self.brand} {self.model}"
     
-my_car = car("suzuki", "shift")
-print(my_car.brand)
-print(my_car.model)
-print(my_car.full_name())
+class ElectricCar(car):
+    def __init__(self, brand, model, battery_size):
+        super().__init__(brand, model)
+        self.battery_size = battery_size
+
+my_tesla = ElectricCar("tesla", "model s", "85 kWh")
+print(my_tesla.full_name())
+
+# my_car = car("suzuki", "shift")
+# print(my_car.brand)
+# print(my_car.model)
+# print(my_car.full_name())
