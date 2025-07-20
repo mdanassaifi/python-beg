afe1afa5641d729bc0453fa5de535e3922bd63a8
mdanassaifi <cscanas533@gmail.com>
2025-07-20 15:13:24 +0530

    xyz

--- decorators/02.py
+++ decorators/02.py
@@ -2,6 +2,7 @@ def debug(func):
     def wrapper(*args, **kwargs):
         args_value = ', '.join(str(arg) for arg in args)
         kwargs_value = ', '.join(f'{key}={value}' for key, value in kwargs.items())
+        print(f"Calling {func.__name__} with args={args_value} and kwargs={kwargs_value}")
         return func(*args, **kwargs)
     
     return wrapper
@@ -10,7 +11,7 @@ def debug(func):
 
 
 
-
+@debug
 def greet(name, greeting="Hello"):
     print(f"{greeting}, {name}!")
 
