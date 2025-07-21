9116b62bae7c0b671bd6230bfcfa1fab78dc9884
mdanassaifi <cscanas533@gmail.com>
2025-07-21 10:40:11 +0530

    xyz

--- error/youtube_manager.py
+++ error/youtube_manager.py
@@ -1,5 +1,18 @@
 
 
+def list_all_videos(videos):
+    pass
+
+def add_video(videos):
+    pass
+
+def update_video(videos):
+    pass
+
+def delete_video(videos):
+    pass
+
+videos = []
 while True:
     print("\n YouTube Manager  | choose an option")
     print("1. List all youtube videos")
@@ -8,3 +21,19 @@ while True:
     print("4. Delete a youtube video")
     print("5. Exit")
     choice = input("Enter your choice: ")
+
+    match choice:
+        case "1":
+            list_all_videos(videos)
+        case "2":
+            add_video(videos)
+        case "3":
+            update_video(videos)
+        case "4":
+            delete_video(videos)
+        case "5":
+            break
+        case _:
+            print("Invalid choice")
+
+
