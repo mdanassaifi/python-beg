3c47058641417c8c5d17ef89423306a3914f5957
mdanassaifi <cscanas533@gmail.com>
2025-07-21 10:27:20 +0530

    xyz

---
+++ error/youtube_manager.py
@@ -0,0 +1,10 @@
+
+
+while True:
+    print("\n YouTube Manager  | choose an option")
+    print("1. List all youtube videos")
+    print("2. Add a youtube video")
+    print("3. update a youtube video")
+    print("4. Delete a youtube video")
+    print("5. Exit")
+    choice = input("Enter your choice: ")
