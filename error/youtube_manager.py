b0af0630d4465ac6c9c35bb60243c798c39faee5
mdanassaifi <cscanas533@gmail.com>
2025-07-21 11:15:34 +0530

    xyz

--- error/youtube_manager.py
+++ error/youtube_manager.py
@@ -12,28 +12,32 @@ def update_video(videos):
 def delete_video(videos):
     pass
 
-videos = []
-while True:
-    print("\n YouTube Manager  | choose an option")
-    print("1. List all youtube videos")
-    print("2. Add a youtube video")
-    print("3. update a youtube video")
-    print("4. Delete a youtube video")
-    print("5. Exit")
-    choice = input("Enter your choice: ")
 
-    match choice:
-        case "1":
-            list_all_videos(videos)
-        case "2":
-            add_video(videos)
-        case "3":
-            update_video(videos)
-        case "4":
-            delete_video(videos)
-        case "5":
-            break
-        case _:
-            print("Invalid choice")
+def main():
+    videos = []
+    while True:
+        print("\n YouTube Manager  | choose an option")
+        print("1. List all youtube videos")
+        print("2. Add a youtube video")
+        print("3. update a youtube video")
+        print("4. Delete a youtube video")
+        print("5. Exit")
+        choice = input("Enter your choice: ")
 
+        match choice:
+            case "1":
+                list_all_videos(videos)
+            case "2":
+                add_video(videos)
+            case "3":
+                update_video(videos)
+            case "4":
+                delete_video(videos)
+            case "5":
+                break
+            case _:
+                print("Invalid choice")
+
+if __name__ == "__main__":
+    main()
 
