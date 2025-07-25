df8f74d193fb67797472cc61664d966b8b36bd24
mdanassaifi <cscanas533@gmail.com>
2025-07-25 14:34:42 +0530

    sqlite3

--- sqlite3/youtube_manager_db.py
+++ sqlite3/youtube_manager_db.py
@@ -23,6 +23,19 @@ def main():
         print("3. Update a video")
         print("4. Delete a video")
         print("5. Exit")
+        choice =input("Enter your choice: ")
+
+        if choice == '1':
+            list_videos()
+        elif choice == '2':
+            name = input("Enter video name: ")
+            time = input("Enter video time: ")
+            add_video(name, time)
+        elif choice == '3':
+            video_id =input("Enter video ID to update: ")
+            name = input("Enter new video name: ")
+            time = input("Enter new video time: ")
+            update_video(video_id,name, time)
 
 if __name__ == "__main__":
     main()
