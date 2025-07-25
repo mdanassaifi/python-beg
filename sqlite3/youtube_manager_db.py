4d19ae6cafa0796dec7de54d69f42fe4e9f60ba3
mdanassaifi <cscanas533@gmail.com>
2025-07-25 14:26:04 +0530

    sqllite3

---
+++ sqlite3/youtube_manager_db.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+conn = sqlite3.connect('youtube_videos.db')
+
+
+cursor = conn.cursor()
+
+
+cursor.execute('''
+
+CREATE TABLE IF NOT EXISTS videos (
+               id INTEGER PRIMARY KEY,
+               name TEXT NOT NULL,
+               time TEXT NOT NULL,
+    )
+''')
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
