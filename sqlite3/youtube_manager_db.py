753fb83ed2a140a354f6a5fce534ef09a1ae7693
mdanassaifi <cscanas533@gmail.com>
2025-07-25 16:44:49 +0530

    sqlite3

--- sqlite3/youtube_manager_db.py
+++ sqlite3/youtube_manager_db.py
@@ -21,15 +21,15 @@ def list_videos():
 
 def add_video(name, time):
     cursor.execute("INSERT INTO videos (name, time) VALUES (?, ?)", (name, time))
-    cursor.commit()
+    conn.commit()
 
 def update_video(video_id, new_name, new_time):
     cursor.execute("UPDATE videos SET name = ?, time = ? WHERE id = ?", (new_name, new_time, video_id))
-    cursor.commit()
+    conn.commit()
 
 def delete_video(video_id):
     cursor.execute("DELETE FROM videos WHERE id = ?", (video_id,))
-    cursor.commit()
+    conn.commit()
 
 def main():
     while True:
