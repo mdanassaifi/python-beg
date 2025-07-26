444a999516160f20c77d6fe9f020d74d1fc9b261
mdanassaifi <cscanas533@gmail.com>
2025-07-26 14:41:03 +0530

    api

--- api/freeapi_username.py
+++ api/freeapi_username.py
@@ -3,7 +3,7 @@ import requests
 def fetch_random_user_freeapi():
     url = "https://api.freeapi.app/api/v1/public/randomusers/user/random"
     response = requests.get(url)
-    data =response.json()
+    data = response.json()
 
 
     if data["success"] and "data" in data:
@@ -14,12 +14,12 @@ def fetch_random_user_freeapi():
     else:
         raise Exception("Failed to fetch user data from FreeAPI")
     
-    def main():
-        try:
-            username, country = fetch_random_user_freeapi()
-            print(f"Username: {username} \n country: {country}")
-        except Exception as e:
-            print(str(e))
+def main():
+    try:
+        username, country = fetch_random_user_freeapi()
+        print(f"username: {username} \ncountry: {country}")
+    except Exception as e:
+        print(str(e))
 
-    if __name__ == "__main__":
-        main()
+if __name__ == "__main__":
+    main()
