43b33dae4a85d3fc01c7c4e323a6c8a314d5cbfc
mdanassaifi <cscanas533@gmail.com>
2025-07-28 13:20:42 +0530

    product-api

---
+++ api/random_product.py
@@ -0,0 +1,26 @@
+import requests
+
+def fetch_random_product():
+    url = "https://api.freeapi.app/api/v1/public/randomproducts/product/random"
+    response = requests.get(url)
+    data = response.json()
+
+
+    if data["success"] and "data" in data:
+        user_data = data["data"]
+        brand = user_data["brand"]
+        category = user_data["category"]
+        description = user_data["description"]
+        return brand, category, description
+    else:
+        raise Exception("Failed to fetch product data from FreeAPI")
+    
+def main():
+    try:
+        brand, category, description = fetch_random_product()
+        print(f"brand: {brand} \ncategory: {category} \ndescription: {description}")
+    except Exception as e:
+        print(str(e))
+
+if __name__ == "__main__":
+    main()
