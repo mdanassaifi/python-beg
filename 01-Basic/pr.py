8d57e8e6763a6fadb47054afe6ad71373b357873
mdanassaifi <cscanas533@gmail.com>
2025-08-03 13:18:57 +0530

    sc

--- 01-Basic/pr.py
+++ 01-Basic/pr.py
@@ -1,3 +1,32 @@
-print("Hello, World!")
-def chai(n):
-    print(n)
+# simple calculator
+
+print("Welcome to the calculator")
+print("Please enter your choice")
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+choice = int(input("Enter your choice: "))
+
+if choice == 1:
+    print("Enter two numbers")
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+    print(num1 + num2)
+elif choice == 2:
+    print("Enter two numbers")
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+    print(num1 - num2)
+elif choice == 3:
+    print("Enter two numbers")
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+    print(num1 * num2)
+elif choice == 4:
+    print("Enter two numbers")
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+    print(num1 / num2)
+else:
+    print("Invalid choice, please try again")
